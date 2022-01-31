1dac3e5abed7b68296a9420c066b5a91d8e66dea
katakurashota <j230347g9327@gmail.com>
2022-01-31 10:08:00 +0000

    bookers2

---
+++ db/migrate/20211108074439_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.text :shop_name
+      t.string :image_id
+      t.text :caption
+      t.integer :user_id
+      
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
